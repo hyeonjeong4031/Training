b01023b72b64e17f44eeafe6d853b498f8a3e6e8
임지원 <9universe9th@gmail.com>
2022-09-06 01:03:07 +0900

    mongoDB연결

---
+++ back/src/db/schemas/project.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const ProjectSchema = new Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    from_date: {
+      type: Date,
+      required: true,
+    },
+    to_date: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProjectModel = model("project", ProjectSchema);
+
+export { ProjectModel };
